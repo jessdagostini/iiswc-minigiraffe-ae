e66c263f0031bb257ede4b47caadef5dc6e35031
Jessica Dagostini <jessicadagostini@gmail.com>
2025-08-22 11:34:41 -0700

    Adding more steps on config env

--- config-env.sh
+++ config-env.sh
@@ -2,6 +2,8 @@
 
 sudo apt-get update && sudo apt-get -y upgrade 
 
-sudo apt-get install -y python3-pip cmake r-base linux-tools-common libcurl4-openssl-dev libssl-dev libfontconfig1-dev libxml2-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
+sudo apt-get install -y python3-pip cmake r-base linux-tools-common linux-tools-6.8.0-64-generic libcurl4-openssl-dev libssl-dev libfontconfig1-dev libxml2-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
+
+sudo sysctl -w kernel.perf_event_paranoid=-1
 
 git clone --recursive https://github.com/jessdagostini/miniGiraffe.git
